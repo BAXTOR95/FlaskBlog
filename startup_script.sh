d0ed1cb8daa96c72c8eb29a092428ff47c648d60
Brian Arriaga <brian.arriaga@gmail.com>
2024-03-18 19:30:43 -0400

    Revert back the render.yaml to use postDeployCommand

--- startup_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create admin user - ensure this is idempotent or checks for existence to avoid errors
-flask --app main.py create-admin "$ADMIN_EMAIL" "$ADMIN_PASSWORD" "$ADMIN_NAME"
-
-# Start the Flask application
-exec flask run --host=0.0.0.0
